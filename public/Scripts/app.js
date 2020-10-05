ed1fbe0c0946387195b0c18ac338daea2cea17f0
Mari <palmer.m07@gmail.com>
2020-10-05 13:06:21 -0400

    added to about page and moved content into content folder

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -4,6 +4,10 @@
         console.log('App Started....');
         $('.carousel').carousel('pause');
 
+ 
+        const viewResumeBTN = document.getElementById("viewResumeBTN");
+        viewResumeBTN.addEventListener('click', ()=>{window.open('/Assets/Marianne_Palmer_Resume.pdf', '_blank', 'fullscreen=yes')});
+
     }
     window.addEventListener('load', Start);
 })();
