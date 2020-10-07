158c2d70b1665445be6f7a921746bfb89032e665
Mari <palmer.m07@gmail.com>
2020-10-07 14:11:24 -0400

    worked on responsivity

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Marianne Palmer' });
 });
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Marianne Palmer' });
 });
 
 /* GET About page. */
