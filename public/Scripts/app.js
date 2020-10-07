0584a22f78909679852effd8ef4b0f9248c3ddd9
Mari <palmer.m07@gmail.com>
2020-10-07 10:56:21 -0400

    added content to services page

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -4,10 +4,15 @@
         console.log('App Started....');
         $('.carousel').carousel('pause');
 
- 
+        // opens resume in a new window ans centers it based on the screen's dimensions
+        function openResume(myURL, title, screenWidth, screenHeight) {
+            let top = (screen.height - screenHeight) / 2;
+            let left = (screen.width - screenWidth) / 2;
+            window.open(myURL, title, 'width=' + screenWidth + ', height=' + screenHeight + ', top=' + top + ', left=' + left);
+        }
+         
         const viewResumeBTN = document.getElementById("viewResumeBTN");
-        viewResumeBTN.addEventListener('click', ()=>{window.open('/Assets/Marianne_Palmer_Resume.pdf', '_blank', 'fullscreen=yes')});
-
+        viewResumeBTN.addEventListener('click', () => openResume('/Assets/Marianne_Palmer_Resume.pdf', 'Marianne Palmer - Resume', 1050, 550)); 
     }
     window.addEventListener('load', Start);
 })();
--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-let app = require('./app');
+let app = require('./config/app');
 let debug = require('debug')('week03:server');
 let http = require('http');
 
