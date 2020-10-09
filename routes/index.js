3c2fa16eb1b62fa7f5a80988ce375f0d0c76ba77
Mari <palmer.m07@gmail.com>
2020-10-09 09:44:05 -0400

    Small change to index router

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Marianne Palmer' });
+  res.render('index', { title: 'Home', heading: 'Marianne Palmer' });
 });
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Marianne Palmer' });
+  res.render('index', { title: 'Home', heading: 'Marianne Palmer' });
 });
 
 /* GET About page. */
